4736a25106c7f10d45953ac3f9bc6e353385e4bf
ajay-chandravanshi <ajaystudy38@gmail.com>
2024-11-21 13:23:59 +0530

    first commit

---
+++ If conditional statement(Example-1).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    cout<<"Enter Two number=";
+    cin>>a;
+    if(a<b)
+    {
+        cout<<a<<" is less then "<<b;
+    }
+    if(a>b)
+    {
+        cout<<a<<" is greater then "<<b;
+    }
+}
